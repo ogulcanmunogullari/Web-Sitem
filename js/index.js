bc18fe213e52db8eca202d41511802b8cec4dfce
Oğulcan <eeogulcan@gmail.com>
2022-04-22 16:45:31 +0300

    Improve Responsive Desing

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const ROTATE_MENU = document.querySelector(".menuCircle")
 const ROTATE_CONTAINER = document.querySelector(".container")
 const SLIDE_ASIDE = document.querySelector("aside")
 const SLIDE_HEADER = document.querySelector(".slider")
+const FOOTER_MOVE = document.querySelector("footer")
 ROTATE_MENU.addEventListener("click", () => {
     
     ROTATE_MENU.classList.toggle("menuCircleRotate")
@@ -13,4 +14,5 @@ ROTATE_MENU.addEventListener("click", () => {
     setTimeout(() => {
         SLIDE_HEADER.classList.toggle("margin")
     }, 450)
+    FOOTER_MOVE.classList.toggle("footerMove")
 })
